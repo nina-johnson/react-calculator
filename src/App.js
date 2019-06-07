36bb582764ad31ad8d261429d7e6595e7a7aef2a
Nina Johnson <9makesthings@gmail.com>
2019-06-06 19:09:42 -0500

    setting up calculator buttons and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="calculator" >
+        <button className="num" >7</button>
+        <button className="num" >8</button>
+        <button className="num" >9</button>
+        <button className="operation" >/</button>
+        <br/>
+
+        <button className="num" >4</button>
+        <button className="num" >5</button>
+        <button className="num" >6</button>
+        <button className="operation" >*</button>
+        <br/>
+
+        <button className="num" >1</button>
+        <button className="num" >2</button>
+        <button className="num" >3</button>
+        <button className="operation" >-</button>
+        <br/>
+
+        <button className="num" >0</button>
+        <button className="num" >.</button>
+        <button className="num" >=</button>
+        <button className="operation" >+</button>
+
+      </div>
     </div>
   );
 }
